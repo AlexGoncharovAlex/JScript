eeec184bcbd420f6da993a85fb84b4b62aacc1fd
John Doe <johndoe@example.com>
2023-05-26 14:19:14 +0300

    new file

--- lesson13/exercise3/getMinSquaredNumber.test.js
+++ lesson13/exercise3/getMinSquaredNumber.test.js
@@ -3,3 +3,11 @@ it('should get min squared number', () => {
   const result = getMinSquaredNumber([-777, 3, -2, 6, 45, -20]);
   expect(result).toEqual(4);
 });
+it('should get min squared number', () => {
+  const result = getMinSquaredNumber([]);
+  expect(result).toEqual(null);
+});
+it('should get min squared number', () => {
+  const result = getMinSquaredNumber('not array');
+  expect(result).toEqual(null);
+});
