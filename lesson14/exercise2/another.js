9ab2e17a577f8c587e19d5dbbdc0fe8e09aaa0ca
John Doe <johndoe@example.com>
2023-06-05 10:35:05 +0300

    add new file

--- lesson14/exercise2/another.js
+++ lesson14/exercise2/another.js
@@ -1,3 +1,18 @@
-import { messanger1 } from './index.js';
+import {
+  createMessenger,
+  sendMessage,
+  setMessage,
+  sendSender,
+} from './index.js';
 
-messanger1.sendMessage('Anna');
+const messanger1 = createMessenger();
+messanger1.sendMessage('Anna'); // ===> Hello, Anna! Just learn it! This message was sent by Gromcod
+
+const messanger2 = createMessenger();
+messanger2.setMessage('You are learning JS and you do it well');
+messanger2.sendMessage('Michael'); // ===> Hello, Michael! You are learning JS and you do it well! This message was sent by Gromcod
+
+const messanger3 = createMessenger();
+messanger3.setMessage('The weather is amazing today');
+messanger3.setSender('Anna');
+messanger3.sendMessage('Alex'); // ===> Hello, Alex! The weather is amazing today! This message was sent by Anna
