15ce790faeca629027085e803713c9e6c7e28ff5
John Doe <johndoe@example.com>
2023-06-06 12:41:07 +0300

    changed file

--- lesson14/exercise3/calculator.js
+++ lesson14/exercise3/calculator.js
@@ -1,11 +1,13 @@
-import { add } from './index.js';
-console.log(add(300));
+// import { add } from './index.js';
+// import { reset } from './index.js';
 
-import { decrease } from './index.js';
+// import { decrease } from './index.js';
+
+// import { getMemo } from './index.js';
+
+import { add, reset, decrease, getMemo } from './index.js';
+
 console.log(decrease(100));
-
-import { reset } from './index.js';
-console.log(reset());
-
-import { getMemo } from './index.js';
+console.log(add(300));
 console.log(getMemo());
+console.log(reset());
--- lesson14/exercise3/index.js
+++ lesson14/exercise3/index.js
@@ -1,21 +1,17 @@
 let memory = 0;
 export function add(number) {
   let result = memory + number;
- return memory = result;
-//   console.log(memory);
+  return (memory = result);
 }
 export function decrease(number) {
   let result = memory - number;
- return memory = result;
-//   console.log(memory);
+  return (memory = result);
 }
 
 export function reset(memory) {
- return memory = 0;
-//    console.log(memory);
+  return (memory = 0);
 }
 
 export function getMemo() {
   return memory;
 }
-
